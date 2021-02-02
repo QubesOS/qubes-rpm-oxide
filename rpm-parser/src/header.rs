a1eae71403b7f1302c2c308b33e886686df74785
Demi Marie Obenour <athena@invisiblethingslab.com>
2021-02-02 14:03:15 -0500

    Move tag data handling to its own module
    Purely a simple refactoring.

--- rpm-parser/src/header.rs
+++ rpm-parser/src/header.rs
@@ -3,6 +3,7 @@
 //! RPM headers use an undocumented binary format.
 #[deny(warnings)]
 use super::ffi::{tag_type, Signature, TagType};
+use super::TagData;
 use openpgp_parser::buffer::Reader;
 use std::convert::TryInto;
 use std::io::{Error, ErrorKind, Read, Result};
@@ -66,89 +67,6 @@ static RPM_SIG_TAGS: &'static [((u32, TagType), Option<usize>, Flags, &'static s
     /// Padding (must be zeroed)
     (1008, TagType::Bin, None, Flags::Zeroed),
 ];
-
-mod tag {
-    /// An RPM tag data entry
-    #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
-    #[repr(C)]
-    pub struct TagData {
-        tag: u32,
-        ty: u32,
-        offset: u32,
-        count: u32,
-    }
-
-    impl TagData {
-        pub const fn new(tag: u32, ty: u32, offset: u32, count: u32) -> Self {
-            Self {
-                tag: u32::to_be(tag),
-                ty: u32::to_be(ty),
-                offset: u32::to_be(offset),
-                count: u32::to_be(count),
-            }
-        }
-
-        /// Cast a slice of [`TagData`] to a slice of `u8`, without copying
-        pub fn as_bytes<'a>(slice: &'a [Self]) -> &'a [u8] {
-            // Static assertions
-            let _: [u8; 16] = [0u8; size_of!(Self)];
-            let _: [u8; 4] = [0u8; size_of!(u32)];
-            let _: [u8; align_of!(u32)] = [0u8; align_of!(Self)];
-            // we now know that `TagData` cannot have any padding
-            unsafe {
-                std::slice::from_raw_parts(
-                    slice.as_ptr() as *const u8,
-                    slice.len() * size_of!(Self),
-                )
-            }
-        }
-        /// Cast a mutable slice of [`TagData`] to a mutable slice of `u8`, without copying
-        ///
-        /// This is safe:
-        ///
-        /// ```compile_fail
-        /// # use rpm_parser::header::TagData;
-        /// let mut i = [TagData::default()];
-        /// let j = TagData::as_bytes_mut(&mut i);
-        /// i[0];
-        /// j[0]; // won’t compile
-        /// ```
-        pub fn as_bytes_mut<'a>(slice: &'a mut [Self]) -> &'a mut [u8] {
-            // Static assertions
-            let _: [u8; 16] = [0u8; size_of!(Self)];
-            let _: [u8; 4] = [0u8; size_of!(u32)];
-            let _: [u8; align_of!(u32)] = [0u8; align_of!(Self)];
-            // we now know that `TagData` cannot have any padding
-            unsafe {
-                std::slice::from_raw_parts_mut(
-                    slice.as_mut_ptr() as *mut u8,
-                    slice.len() * size_of!(Self),
-                )
-            }
-        }
-    }
-
-    impl TagData {
-        /// The tag
-        pub fn tag(&self) -> u32 {
-            u32::from_be(self.tag)
-        }
-        /// The type
-        pub fn ty(&self) -> u32 {
-            u32::from_be(self.ty)
-        }
-        /// The offset
-        pub fn offset(&self) -> u32 {
-            u32::from_be(self.offset)
-        }
-        /// The count
-        pub fn count(&self) -> u32 {
-            u32::from_be(self.count)
-        }
-    }
-}
-
-pub use tag::TagData;
 
 /// A parsed RPM header
 #[non_exhaustive]
---
+++ rpm-parser/src/tagdata.rs
@@ -0,0 +1,78 @@
+//! Routines for handling RPM tag data entries
+
+/// An RPM tag data entry
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
+#[repr(C)]
+pub struct TagData {
+    tag: u32,
+    ty: u32,
+    offset: u32,
+    count: u32,
+}
+
+impl TagData {
+    pub const fn new(tag: u32, ty: u32, offset: u32, count: u32) -> Self {
+        Self {
+            tag: u32::to_be(tag),
+            ty: u32::to_be(ty),
+            offset: u32::to_be(offset),
+            count: u32::to_be(count),
+        }
+    }
+
+    /// Cast a slice of [`TagData`] to a slice of `u8`, without copying
+    pub fn as_bytes<'a>(slice: &'a [Self]) -> &'a [u8] {
+        // Static assertions
+        let _: [u8; 16] = [0u8; size_of!(Self)];
+        let _: [u8; 4] = [0u8; size_of!(u32)];
+        let _: [u8; align_of!(u32)] = [0u8; align_of!(Self)];
+        // we now know that `TagData` cannot have any padding
+        unsafe {
+            std::slice::from_raw_parts(slice.as_ptr() as *const u8, slice.len() * size_of!(Self))
+        }
+    }
+
+    /// Cast a mutable slice of [`TagData`] to a mutable slice of `u8`, without copying
+    ///
+    /// This is safe:
+    ///
+    /// ```compile_fail
+    /// # use rpm_parser::header::TagData;
+    /// let mut i = [TagData::default()];
+    /// let j = TagData::as_bytes_mut(&mut i);
+    /// i[0];
+    /// j[0]; // won’t compile
+    /// ```
+    pub fn as_bytes_mut<'a>(slice: &'a mut [Self]) -> &'a mut [u8] {
+        // Static assertions
+        let _: [u8; 16] = [0u8; size_of!(Self)];
+        let _: [u8; 4] = [0u8; size_of!(u32)];
+        let _: [u8; align_of!(u32)] = [0u8; align_of!(Self)];
+        // we now know that `TagData` cannot have any padding
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                slice.as_mut_ptr() as *mut u8,
+                slice.len() * size_of!(Self),
+            )
+        }
+    }
+}
+
+impl TagData {
+    /// The tag
+    pub fn tag(&self) -> u32 {
+        u32::from_be(self.tag)
+    }
+    /// The type
+    pub fn ty(&self) -> u32 {
+        u32::from_be(self.ty)
+    }
+    /// The offset
+    pub fn offset(&self) -> u32 {
+        u32::from_be(self.offset)
+    }
+    /// The count
+    pub fn count(&self) -> u32 {
+        u32::from_be(self.count)
+    }
+}
